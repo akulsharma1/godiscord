54eb76b02b1363a9670de4d057bdd33e42608c35
akulsharma1 <akulsharma02@gmail.com>
2021-06-11 13:26:28 -0700

    Added print webhook send response code

--- embeds.go
+++ embeds.go
@@ -1,12 +1,14 @@
 package godiscord
 
 import (
+	"fmt"
 	"bytes"
 	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
 	"strings"
+	"io/ioutil"
 )
 
 //Embed is a struct representing a Discord embed object
@@ -143,9 +145,13 @@ func (e *Embed) SendToWebhook(Webhook string) error {
 	if marshalErr != nil {
 		return marshalErr
 	}
-	_, postErr := http.Post(Webhook, "application/json", bytes.NewBuffer(embed))
+
+	req, postErr := http.NewRequest(Webhook, "application/json", bytes.NewBuffer(embed))
 	if postErr != nil {
 		return postErr
 	}
+	defer req.Body.Close()
+	pJson, _ := ioutil.ReadAll(req.Body)
+	fmt.Println(string(pJson))
 	return nil
 }
